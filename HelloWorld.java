0e3fec1234fc53933a0963c369cec21adee2c400
Jimmy <jmasaro3@gmail.com>
2025-02-16 18:28:40 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,20 @@
 public class Helloworld
-(
+{
    public static void main(String[] args)
    {
       system.out.println("Hello world!");
-   }
-)
+      
+      //name is a reference variable
+      String name = "George Washignton";
+      
+      Systme.out.println(name);
+      
+      //age is a primitive variable
+      int age= 24;
+      
+      System.out.println(age);
+      
+      
+  }//end main
+}//end class
+      
